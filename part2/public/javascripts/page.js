81d2a2d9019fbb149a382ad7b33d96fea626e5a0
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 08:03:16 +0000

    [automatic commit] 2025-06-20 08:03:16 UTC 180.150.112.144

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -187,7 +187,7 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            const response = JSON.parse(xht)
+            const response = JSON.parse(xmlhttp.responseText);
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
