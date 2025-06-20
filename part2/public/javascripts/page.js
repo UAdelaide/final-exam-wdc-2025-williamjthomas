e55f4d221a3b38d25d60ec335b63144e4a3aa137
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:47:44 +0000

    [automatic commit] 2025-06-20 07:47:44 UTC 180.150.112.144

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -177,8 +177,8 @@ function downvote(index) {
 function login(){
 
     let user = {
-        user: document.getElementById('username').value,
-        pass: document.getElementById('password').value
+        username: document.getElementById('username').value,
+        password: document.getElementById('password').value
     };
 
     // Create AJAX Request
