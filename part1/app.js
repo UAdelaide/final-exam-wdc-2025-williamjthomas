83794ec6717fc0ec17e715dbededbf985b1d6e81
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:40:15 +0000

    [automatic commit] 2025-06-20 02:40:15 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -17,13 +17,13 @@ let db;
         if (rows[0].count === 0) {
             await db.execute(`
                 INSERT INTO Users
-        (username, email, password_hash, role)
-        VALUES
-        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-        ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
-        ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
+                (username, email, password_hash, role)
+                VALUES
+                ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+                ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+                ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+                ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
+                ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
       `);
         }
     }
