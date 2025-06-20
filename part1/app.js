f0464b12c893f5bb8404083782e33ee481e75c82
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:39:34 +0000

    [automatic commit] 2025-06-20 02:39:34 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -6,28 +6,27 @@ let db;
 
 (async () => {
     try {
-    db = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'DogWalkService'
-    });
+        db = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'DogWalkService'
+        });
 
-    const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
-    if (rows[0].count === 0) {
-      await db.execute(`
+        const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
+        if (rows[0].count === 0) {
+            await db.execute(`
         INSERT INTO Users
-        (
-    username, email, password_hash, role)
-    VALUES (
-    'alice123', 'alice@example.com', 'hashed123', 'owner'),
-    ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-    ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
-    ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
+        (username, email, password_hash, role)
+        VALUES
+        ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+        ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+        ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+        ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
+        ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
       `);
+        }
     }
-}
 })
 
 
