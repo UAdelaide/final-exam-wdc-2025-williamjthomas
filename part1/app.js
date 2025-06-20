dd17782c197596e1737f706d2b4e75cadc60135f
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:45:01 +0000

    [automatic commit] 2025-06-20 02:45:00 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -16,8 +16,7 @@ let db;
         const [UsersRows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
         if (UsersRows[0].count === 0) {
             await db.execute(`
-                INSERT INTO Users
-                (username, email, password_hash, role)
+                INSERT INTO Users (username, email, password_hash, role)
                 VALUES
                 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
                 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
