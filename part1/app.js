df6bb0f98cf7465bcf299824b1354801ccbbdb29
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 03:31:34 +0000

    [automatic commit] 2025-06-20 03:31:33 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -77,7 +77,7 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-        const [walkRequests] = await db.execute("SELECT ")
+        const [walkRequests] = await db.execute("SELECT FROM WalkRequests ")
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch walk requests' });
     }
