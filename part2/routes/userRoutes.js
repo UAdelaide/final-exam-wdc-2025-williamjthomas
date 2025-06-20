6d6c5bae8d3d5a47c8ea4ca2c25ac865f861dbcc
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:52:08 +0000

    [automatic commit] 2025-06-20 07:52:08 UTC 180.150.112.144

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -49,6 +49,10 @@ router.post('/login', async (req, res) => {
       return res.status(401).json({ error: 'Invalid credentials' });
     }
 
+    const user = rows[0];
+
+    if (user.)
+
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
     res.status(500).json({ error: 'Login failed' });
