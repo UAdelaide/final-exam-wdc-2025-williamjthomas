547c6fa577dd121d3c9e2cbc72286388fa3774a7
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:58:35 +0000

    [automatic commit] 2025-06-20 02:58:35 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -61,7 +61,7 @@ let db;
 
 app.get('/api/dogs', async (req, res) => {
     try {
-        const [dogs] = await db.execute("SELECT d.name AS dog_name, d.size, ")
+        const [dogs] = await db.execute("SELECT d.name AS dog_name, d.size, u.username AS owner_username")
     } catch (err) {
         res.status(500).json({ error: 'Failed to fetch dogs'})
     }
