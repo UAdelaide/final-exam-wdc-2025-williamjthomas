12592e3bec42a501f5c5e1b2e7084b33d8802b8a
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:25:55 +0000

    [automatic commit] 2025-06-20 07:25:55 UTC 180.150.112.144

--- part2/app.js
+++ part2/app.js
@@ -2,15 +2,15 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 
-// Routes
-const walkRoutes = require('./routes/walkRoutes');
-const userRoutes = require('./routes/userRoutes');
-
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+
+// Routes
+const walkRoutes = require('./routes/walkRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
