e2cc1c6c6e2e26b218178a848c381c4c5e3d3d7d
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:32:23 +0000

    [automatic commit] 2025-06-20 07:32:23 UTC 180.150.112.144

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -195,7 +195,7 @@ function login(){
 
     // Open connection to server & send the post data using a POST request
     // We will cover POST requests in more detail in week 8
-    xmlhttp.open("POST", "/users/login", true);
+    xmlhttp.open("POST", "/api/users/login", true);
     xmlhttp.setRequestHeader("Content-type", "application/json");
     xmlhttp.send(JSON.stringify(user));
 
