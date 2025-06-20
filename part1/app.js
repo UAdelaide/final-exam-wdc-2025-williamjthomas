208e03545f8e175ebcf3ca5ffb6c6caccd0fd7c2
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 03:57:28 +0000

    [automatic commit] 2025-06-20 03:57:28 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -78,7 +78,7 @@ let db;
             await db.execute(`
                 INSERT INTO WalkRatings (request_id, walker_id, owner_id, rating, comments)
                 VALUES
-                ()
+                (SELECT request_id FROM WalkRequests WHERE duration_minutes = 10)
                 `);
         }
     } catch (err) {
