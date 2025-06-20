b1c03acb3edd5871df80da27e4b37100163a274c
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 03:52:21 +0000

    [automatic commit] 2025-06-20 03:52:21 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -68,7 +68,7 @@ let db;
             await db.execute(`
                 INSERT INTO WalkApplications (request_id, walker_id, status)
                 VALUES
-                
+                ()
             `)
         }
 
