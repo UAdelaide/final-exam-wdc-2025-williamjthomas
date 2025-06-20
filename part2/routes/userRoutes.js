414e6e6c4353551982cddd09d5cca975e3285448
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:09:13 +0000

    [automatic commit] 2025-06-20 07:09:13 UTC 180.150.112.144

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -37,6 +37,7 @@ router.get('/me', (req, res) => {
 
 // POST login (dummy version)
 router.post('/login', async (req, res) => {
+  console.log
   const { username, password } = req.body;
 
   try {
