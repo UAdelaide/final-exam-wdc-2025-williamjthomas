c4aff706403b5c0bc66d0ba0687832c4299b8d7c
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:39:14 +0000

    [automatic commit] 2025-06-20 02:39:14 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -16,10 +16,15 @@ let db;
     const [rows] = await db.execute('SELECT COUNT(*) AS count FROM Users');
     if (rows[0].count === 0) {
       await db.execute(`
-        INSERT INTO books (title, author) VALUES
-        ('1984', 'George Orwell'),
-        ('To Kill a Mockingbird', 'Harper Lee'),
-        ('Brave New World', 'Aldous Huxley')
+        INSERT INTO Users
+        (
+    username, email, password_hash, role)
+    VALUES (
+    'alice123', 'alice@example.com', 'hashed123', 'owner'),
+    ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+    ('carol123', 'carol@example.com', 'hashed789', 'owner'),
+    ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
+    ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
       `);
     }
 }
