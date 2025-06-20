74a8a64b0f6746ffe4d2dad28d1ba4f055788fe1
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 07:22:12 +0000

    [automatic commit] 2025-06-20 07:22:12 UTC 180.150.112.144

--- part2/app.js
+++ part2/app.js
@@ -10,6 +10,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({}))
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.use('/api/walks', walkRoutes);
