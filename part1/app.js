46e8dd5a8e4b899e182985c28f830b1ba7172957
williamjthomas <a1922353@adelaide.edu.au>
2025-06-20 02:44:20 +0000

    [automatic commit] 2025-06-20 02:44:20 UTC 180.150.112.144

--- part1/app.js
+++ part1/app.js
@@ -30,14 +30,13 @@ let db;
         const [DogsRows] = await db.execute('SELECT COUNT(*) AS count FROM Dogs');
         if (DogsRows[0].count === 0) {
             await db.execute(`
-                INSERT INTO Users
-                (username, email, password_hash, role)
-                VALUES
-                ('alice123', 'alice@example.com', 'hashed123', 'owner'),
-                ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
-                ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-                ('jameswalker', 'james@example.com', 'hashed101112', 'walker'),
-                ('caroline123', 'caroline@example.com', 'hashed131415', 'owner');
+                INSERT INTO Dogs (owner_id, name, size)
+    VALUES
+    ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Max', 'medium'),
+    ((SELECT user_id FROM Users WHERE username = 'carol123'), 'Bella', 'small'),
+    ((SELECT user_id FROM Users WHERE username = 'caroline123'), 'Toby', 'large'),
+    ((SELECT user_id FROM Users WHERE username = 'caroline123'), 'Sam', 'medium'),
+    -> ((SELECT user_id FROM Users WHERE username = 'alice123'), 'Mel', 'small');
             `);
         }
     } catch (err) {
