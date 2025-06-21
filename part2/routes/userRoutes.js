ec77abbab53fd8847d2092c6e787eedf5bc7b4b8
williamjthomas <a1922353@adelaide.edu.au>
2025-06-21 05:07:43 +0000

    [automatic commit] 2025-06-21 05:07:43 UTC 180.150.112.144

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -96,7 +96,7 @@ router.get('/dogs', async (req, res) => {
   }
 
   try {
-    const [dogs] = await db.query("SELECT d.name FROM Dogs d JOIN Users u ON d.owner_id = u.user_id");
+    const [dogs] = await db.query("SELECT dogname FROM Dogs d JOIN Users u ON d.owner_id = u.user_id");
     res.json(dogs);
   } catch (err) {
     console.error(err);
