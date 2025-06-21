379662dd941c76a0edfb4abd26fab58b7bd1170a
williamjthomas <a1922353@adelaide.edu.au>
2025-06-21 06:31:48 +0000

    [automatic commit] 2025-06-21 06:31:48 UTC 180.150.112.144

--- part2/app.js
+++ part2/app.js
@@ -29,5 +29,7 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
+
+
 // Export the app instead of listening here
 module.exports = app;
