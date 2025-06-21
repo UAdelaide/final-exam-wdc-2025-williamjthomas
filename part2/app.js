cad0b383440027d4275bfb41730b990faefbd2e7
williamjthomas <a1922353@adelaide.edu.au>
2025-06-21 06:32:08 +0000

    [automatic commit] 2025-06-21 06:32:08 UTC 180.150.112.144

--- part2/app.js
+++ part2/app.js
@@ -29,7 +29,14 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-
+app.get('/api/dogs', async (req, res) => {
+    try {
+        const [dogs] = await db.execute("SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id");
+        res.json(dogs);
+    } catch (err) {
+        res.status(500).json({ error: 'Failed to fetch dogs' });
+    }
+});
 
 // Export the app instead of listening here
 module.exports = app;
