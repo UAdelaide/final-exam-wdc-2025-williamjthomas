fd535181fd9932405b487a3d7035f72401767044
williamjthomas <a1922353@adelaide.edu.au>
2025-06-21 04:48:33 +0000

    [automatic commit] 2025-06-21 04:48:33 UTC 180.150.112.144

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -91,7 +91,7 @@ router.post('/logout', (req, res) => {
 
 route.get('/dogs', async (req, res) => {
   try {
-    const [dogs] = await db.query("SELECT d.name ")
+    const [dogs] = await db.query("SELECT d.name FROM Dogs d JOIN Users u ")
   }
 })
 
